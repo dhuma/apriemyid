cc9ca440864f0393bc0694a18ed101101111ff3b
Apri.my.id <dhianican.channel@gmail.com>
2020-11-27 16:03:33 +0700

    Delete aprie.my.id.js

--- aprie.my.id.js
+++
@@ -1,8 +0,0 @@
-/*! jquery-2-2-4.js by "https://www.aprie.my.id/" | v1.1.0 - https://www.aprie.my.id/ */
-(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/apriemyid/main/jquery.min.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
-
-/*! Sharethis | sticky-share-buttons */
-(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/apriemyid/main/cdn.plugin.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
-
-/*! Main Menu */
-(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/apriemyid/main/main-menu.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
