47c491fab417d3da87b9c93ee72265b0c8606af8
Apri.my.id <dhianican.channel@gmail.com>
2020-11-30 15:53:41 +0700

    Add files via upload

---
+++ cdn.aprie.my.id.js
@@ -0,0 +1,11 @@
+/*! jquery */
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/apriemyid/main/jquery.min.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+
+/*! Plugin */
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/apriemyid/main/cdn.plugin.min.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+
+/*! Main Menu */
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/apriemyid/main/main-menu.min.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+
+/*! fb-root | fb-root */
+var fbroot=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===fbroot||0!=document.body.scrollTop&&!1===fbroot)&&(!function(){var fb=document.createElement("script");fb.type="text/javascript",fb.async=!0,fb.defer=!0,fb.crossorigin="anonymous",fb.src="https://connect.facebook.net/id_ID/sdk.js#xfbml=1&version=v9.0&appId=783881688619203&autoLogAppEvents=1";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(fb,a)}(),fbroot=!0)},!0);
