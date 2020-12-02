ea415b1a2ee161d567a401cd4eba1677776c8540
Apri.my.id <dhianican.channel@gmail.com>
2020-12-02 22:33:32 +0700

    Add files via upload

---
+++ static.cdn.aprie.my.id.js
@@ -0,0 +1,8 @@
+/*! jquery */
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/apriemyid/main/jquery.min.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+
+/*! Plugin */
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/apriemyid/main/cdn.aprie.my.id.plugin.min.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+
+/*! Main Menu */
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/apriemyid/main/cdn.aprie.my.id.main.menu.min.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
